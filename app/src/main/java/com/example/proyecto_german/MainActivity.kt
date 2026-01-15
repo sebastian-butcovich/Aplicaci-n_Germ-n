f13653d057a000e9f371c491b774f94f5f45ec06
sebastian <sebabutcovich@gmail.com>
2026-01-15 20:25:19 -0300

    Navegación básica Se puede pasar entre una vista home y una vista de formulario de perforación

--- app/src/main/java/com/example/proyecto_german/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_german/MainActivity.kt
@@ -5,22 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.proyecto_german.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var currentFragment: Fragment
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val formFragment = FormFragment()
-        supportFragmentManager.beginTransaction().add(R.id.nav_host,formFragment).commit()
+        enableEdgeToEdge()
+        setupBottomNav()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun setupBottomNav() {
+        val homeFragment = HomeFragment()
+        val formFragment = FormFragment()
+        currentFragment=homeFragment
+        with(supportFragmentManager) {
+            beginTransaction()
+            .add(R.id.nav_host, formFragment)
+            .commit()
+            beginTransaction()
+                .add(R.id.nav_host, homeFragment)
+                .hide(formFragment).commit()
+            binding.navView.setOnItemSelectedListener { menuItem->
+                when(menuItem.itemId){
+                    R.id.navigation_home->{
+                        beginTransaction().hide(formFragment).show(homeFragment).commit()
+                        currentFragment = homeFragment
+                    }
+                    R.id.navigation_registro->{
+                        beginTransaction().hide(homeFragment).show(formFragment).commit()
+                        currentFragment = formFragment
+                    }
+                }
+                true
+            }
         }
     }
 }
