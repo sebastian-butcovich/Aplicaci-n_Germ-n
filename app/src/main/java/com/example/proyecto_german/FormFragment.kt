306860b9a35bea0c003c175297fac9b381a8e0c2
sebastian <sebabutcovich@gmail.com>
2026-01-15 19:29:46 -0300

    Primer commit

---
+++ app/src/main/java/com/example/proyecto_german/FormFragment.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto_german
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.proyecto_german.databinding.FragmentFormularioPerforacionBinding
+
+class FormFragment: Fragment() {
+    private var _biding :FragmentFormularioPerforacionBinding? =null
+    private val binding get() = _biding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _biding = FragmentFormularioPerforacionBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
