91b563e0e62a97288f69b67520fc8c790de47960
Sebastián Butcovich <sebabutcovich@gmail.com>
2026-01-21 13:28:52 -0300

    Hay un formulario para agregar detalles de la perforación

--- app/src/main/java/com/example/proyecto_german/FormFragment.kt
+++ app/src/main/java/com/example/proyecto_german/FormFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.proyecto_german.databinding.FragmentFormularioPerforacionBinding
+import com.example.proyecto_german.databinding.FragmentFormularioPerforacionProfundidadBinding
 
 class FormFragment: Fragment() {
     private var _biding :FragmentFormularioPerforacionBinding? =null
@@ -16,6 +19,25 @@ class FormFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _biding = FragmentFormularioPerforacionBinding.inflate(inflater,container,false)
+        accionBoton()
         return binding.root
+    }
+    fun accionBoton(){
+        print("LLegue aca 1")
+        val formFragment = FormFragmentProfundidad()
+        val miBoton = binding.root.findViewById<Button>(R.id.boton_agregar_profundiad)
+        miBoton.setOnClickListener {
+            // Código al hacer clic
+//            Toast.makeText(requireContext(), "¡Botón clickeado!", Toast.LENGTH_SHORT).show()
+            if(formFragment.isVisible){
+                print("Llegue aca")
+                childFragmentManager.beginTransaction()
+                    .remove( formFragment).commit()
+            }else{
+                print(" O llegue aca")
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.contenedor_frame_layout,formFragment).commit()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/proyecto_german/OnClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto_german
+
+interface OnClickListener {
+    fun onClick()
+}
